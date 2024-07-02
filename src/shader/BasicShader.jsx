ad73f0b7e6b4816804703e4d607b77971f9a40ed
xuchaozou <52873083+xuchaozou@users.noreply.github.com>
2024-07-02 23:33:46 +0800

    feat: 测试shader

---
+++ src/shader/BasicShader.jsx
@@ -0,0 +1,67 @@
+import { Space } from 'antd';
+import { Cesium3DTile, Cesium3DTileset, HeadingPitchRange, Viewer, Math, ScreenSpaceEventHandler, ScreenSpaceEventType, Cesium3DTileFeature, defined, Color, Cartesian3, HeadingPitchRoll, PostProcessStage, PostProcessStageLibrary, Primitive, GeometryInstance, RectangleGeometry, Rectangle, EllipsoidSurfaceAppearance, Material } from 'cesium';
+import Container from '../components/Container';
+import { useRef, useState, useEffect } from 'react';
+
+
+const BasicShader = props => {
+    const viewerRef = useRef(null);
+
+    /**
+     * 
+     * @param {Viewer} viewer 
+     */
+    const loadedMap = (viewer) => {
+
+        debugger
+
+        const customMaterial = new Material({
+            fabric: {
+                type: "custom1",
+                uniforms : {
+                    bottomColor: new Color(0.6, 0.0, 0.0, 1.0), // 底部颜色
+                    topColor: new Color(0.0, 0.6, 1.0, 1.0) // 顶部颜色
+                },
+                source: `
+                    uniform vec4 bottomColor;
+                                 uniform vec4 topColor;
+                     czm_material czm_getMaterial(czm_materialInput materialInput)
+                    {
+                        czm_material material = czm_getDefaultMaterial(materialInput);
+                        
+                        vec2 st = materialInput.st;
+                        float t = st.y; // 使用纹理坐标的y作为渐变因子，从底部到顶部渐变
+                        
+                        // 线性插值计算颜色
+                        material.diffuse = czm_mix(bottomColor.rgb, topColor.rgb, t);
+                        
+                        return material;
+                    }
+                `
+            }
+        })
+
+        const rectangle = viewer.scene.primitives.add(
+            new Primitive({
+                geometryInstances: new GeometryInstance({
+                    geometry: new RectangleGeometry({
+                        rectangle: Rectangle.fromDegrees(116, 39, 117, 39.7),
+                        vertexFormat: EllipsoidSurfaceAppearance.VERTEX_FORMAT
+                    })
+                }),
+                appearance: new EllipsoidSurfaceAppearance({
+                    aboveGround: true,
+                    material : customMaterial
+                })
+            })
+        )
+        //    viewer.zoomTo(rectangle)
+        viewer.camera.flyTo({
+            destination: new Cartesian3.fromDegrees(116, 39, 1000)
+        })
+    }
+
+    return (<Container loadedMap={loadedMap}> </Container>)
+}
+
+export default BasicShader
---
+++ src/shader/index.md
@@ -0,0 +1,10 @@
+---
+title: shader
+description: shader
+nav: cesium
+group: cesium示例
+order: 4
+---
+
+## 基础shader
+<code src="./BasicShader.jsx"></code>
